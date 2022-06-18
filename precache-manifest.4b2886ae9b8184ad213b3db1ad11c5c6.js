4b8872dac7cf09eb2c22b4e19e1b76545b89ede8
Code Sandbox <hello@codesandbox.io>
2022-06-18 08:47:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4b2886ae9b8184ad213b3db1ad11c5c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecdd8e423d7f2f77222079c98aa615d",
+    "url": "https://rohansingh3121.github.io/csb-611m8u/index.html"
+  },
+  {
+    "revision": "e34a5502d867040868d7",
+    "url": "https://rohansingh3121.github.io/csb-611m8u/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "75948bd5ad7966ec5f03",
+    "url": "https://rohansingh3121.github.io/csb-611m8u/static/css/main.63d69f3f.chunk.css"
+  },
+  {
+    "revision": "e34a5502d867040868d7",
+    "url": "https://rohansingh3121.github.io/csb-611m8u/static/js/2.a00bcadf.chunk.js"
+  },
+  {
+    "revision": "952c689b7d860a84fc37d31aea8d3e75",
+    "url": "https://rohansingh3121.github.io/csb-611m8u/static/js/2.a00bcadf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75948bd5ad7966ec5f03",
+    "url": "https://rohansingh3121.github.io/csb-611m8u/static/js/main.a6f70ac2.chunk.js"
+  },
+  {
+    "revision": "8293d9897e09621e614e",
+    "url": "https://rohansingh3121.github.io/csb-611m8u/static/js/runtime-main.17e5e7d7.js"
+  },
+  {
+    "revision": "c8901400b80365e19cc771afc992050a",
+    "url": "https://rohansingh3121.github.io/csb-611m8u/static/media/Rohan Singh-Intern.c8901400.pdf"
+  }
+]);
